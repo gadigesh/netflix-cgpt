d9476fb8c079e1f43e96c9058b85b3274dc12956
GADIGESH HIREMATH <98770444+gadigesh@users.noreply.github.com>
2025-06-29 02:31:35 +0530

    Update Constant.js

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -14,12 +14,11 @@ export const API_OPTIONS = {
 };
 
 export const IMG_CDNURL = "https://image.tmdb.org/t/p/w500";
+export const OPENAI_KEY =
+  "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";
 
 export const SUPPORTED_LANG = [
   { identifier: "En", name: "English" },
   { identifier: "Kannada", name: "Kannada" },
   { identifier: "Spanish", name: "Spanish" },
 ];
-
-export const OPENAI_KEY =
-  "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";
