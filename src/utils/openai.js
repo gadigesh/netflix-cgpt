2e539a2eb734ac88be3746c9afa5847d1d14d39d
GADIGESH HIREMATH <98770444+gadigesh@users.noreply.github.com>
2025-06-29 10:40:38 +0530

    Update openai.js

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 import { OPENAI_KEY } from "./Constant";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_KEY, // This is the default and can be omitted
+  apiKey: process.env.OPENAI_API_KEY, // This is the default and can be omitted
   dangerouslyAllowBrowser: true,
 });
 
